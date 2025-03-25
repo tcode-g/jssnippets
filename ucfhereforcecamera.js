ba94e06b7247f04a6b9f6e2b1a6073ad1fb8f71a
Trevor <68875146+tcode-g@users.noreply.github.com>
2025-03-25 15:48:55 -0400

    version change

--- ucfhereforcecamera.js
+++ ucfhereforcecamera.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UCF Here Force Camera
 // @namespace    https://staybrowser.com/
-// @version      0.9
+// @version      0.01
 // @description  Template userscript created by Stay
 // @author       You
 // @match        tcode.github.io/*
